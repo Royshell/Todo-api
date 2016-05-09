a10b23140eaa377ead80eaeb4433e315bb7a7cc3
roys <roy.shell@netomedia.com>
2016-05-09 16:33:09 +0300

    Conver GET /todos/:id to sequelize

--- server.js
+++ server.js
@@ -42,56 +42,45 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var mathedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (mathedTodo) {
-		res.json(mathedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function(todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		}else {
+			res.status(404).send();
+		}
+		
+	}).catch(function(e) {
+		res.status(500).send();
+	});
 });
 
 // POST /todos/
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body)
-		.then(function(todo) {
-			res.status(200).json(todo.toJSON());
-		}).catch(function(e) {
-			res.status(404).json(e);
-		});
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400.).send();
-	// }
-
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
-
-	// //push body into array
-	// todos.push(body);
-
-	// res.json(body);
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON());
+	}).catch(function(e) {
+		res.status(404).json(e);
+	});
 });
 
 // DELETE /todos/:id 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var mathedTodo = _.findWhere(todos, {
-		id: todoId
-	});
+	// var mathedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
 
-	if (!mathedTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		})
-	} else {
-		todos = _.without(todos, mathedTodo);
-		res.json(mathedTodo);
-	}
+	// if (!mathedTodo) {
+	// 	res.status(404).json({
+	// 		"error": "no todo found with that id"
+	// 	})
+	// } else {
+	// 	todos = _.without(todos, mathedTodo);
+	// 	res.json(mathedTodo);
+	// }
 });
 
 // PUT  /todos/:id
