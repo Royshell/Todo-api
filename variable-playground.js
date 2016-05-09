8e80fe0674e2a4f30b92c58f5b3e0f356b8b4e53
roys <roy.shell@netomedia.com>
2016-05-09 16:05:27 +0300

    Add todo model and post todo

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: 'Roy',
+// 	age: 31
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Roy',
+// 	// 	age: 32
+// 	// };
+// 	obj.age = 32;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+//Array Example  
+
+var grades = [15, 88];
+
+function addGrades(gradesArr) {
+
+	gradesArr.push(55);
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
