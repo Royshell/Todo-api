a39bf4990dd31af7c44772f45c659b149881b740
roys <roy.shell@netomedia.com>
2016-07-11 17:14:34 +0300

    Convert CRUS request to use current user todos only

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ app.get('/', function(req, res) {
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var queryParams = req.query;
 
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	};
+
+
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		where.completed = true;
@@ -52,7 +56,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -86,7 +95,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: [todoId]
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -102,7 +112,7 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 });
 
 // PUT  /todos/:id
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	var body = _.pick(req.body, 'description', 'completed');
@@ -117,7 +127,12 @@ app.put('/todos/:id', function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
@@ -165,6 +180,8 @@ app.get('/users', function(req, res) {
 	var queryParams = req.query;
 
 	var where = {};
+
+	where.userId = req.user.get('id');
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		where.completed = true;
