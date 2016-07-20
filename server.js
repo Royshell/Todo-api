bc85c71c01b9f442d2de812ae5a17c9a85409fdc
roys <roy.shell@netomedia.com>
2016-07-20 17:53:40 +0300

    Added DELETE /users/login

--- server.js
+++ server.js
@@ -161,73 +161,31 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
+		userInstance = user;
 
-	}, function() {
+		return db.token.create({
+			token: token
+		});
+
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function() {
 		res.status(401).send();
 	});
 });
 
-// GET /users?completed=true&q=work
-app.get('/users', function(req, res) {
-	var queryParams = req.query;
 
-	var where = {};
-
-	where.userId = req.user.get('id');
-
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		where.completed = true;
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		where.completed = false;
-	}
-
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-
-		where.description = {
-			$like: '%' + queryParams.q + '%'
-		};
-	}
-
-	db.user.findAll({
-		where: where
-	}).then(function(users) {
-		if (!!users) {
-			res.json(users);
-		} else {
-			console.log("No users found");
-		}
-	}).catch(function(e) {
-		res.status(500).json(e);
-	});
-});
-
-// DELETE /users/:id 
-app.delete('/users/:id', function(req, res) {
-	var userId = parseInt(req.params.id, 10);
-
-	db.user.destroy({
-		where: {
-			id: [userId]
-		}
-	}).then(function(rowsDeleted) {
-		if (rowsDeleted === 0) {
-			res.status(404).json({
-				error: 'No todo with id'
-			});
-		} else {
-			res.status(204).send();
-		}
-	}).catch(function(e) {
+// POST /users/login 
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}).catch(function () {
 		res.status(500).send();
-	});
+	})
 });
 
 db.sequelize.sync({
